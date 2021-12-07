0575d1edd1a6a34b5fd1cd5b9808889818cc2908
EdsonHTJ <edsonsobralense@gmail.com>
2021-12-07 00:23:16 -0300

    add hash

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -10,12 +13,51 @@ import (
 
 var cache ttlcache.SimpleCache = ttlcache.NewCache()
 
+type writeReq struct {
+	Hash  string
+	Name  string
+	Value interface{}
+}
+
 func main() {
 	cache.SetTTL(time.Duration(10 * time.Second))
 
 	app := fiber.New()
 
-	app.Get("/test",)
-	cache.Set("1", 6868985)
-	fmt.Println(cache.Get("1"))
+	app.Post("/write", func(c *fiber.Ctx) error {
+		// Get raw body from POST request
+		c.Body() // user=john
+		fmt.Println(c.Body())
+
+		var req writeReq
+
+		json.Unmarshal(c.Body(), &req)
+
+		hash := sha256.Sum256(append([]byte(req.Name), []byte(req.Hash)...))
+		cache.Set(hex.EncodeToString(hash[:]), req.Value)
+
+		return nil
+	})
+
+	app.Post("/read", func(c *fiber.Ctx) error {
+		// Get raw body from POST request
+		c.Body() // user=john
+		fmt.Println(c.Body())
+
+		var req writeReq
+		fmt.Println(string(c.Body()))
+		json.Unmarshal(c.Body(), &req)
+
+		hash := sha256.Sum256(append([]byte(req.Name), []byte(req.Hash)...))
+
+		req.Value, _ = cache.Get(hex.EncodeToString(hash[:]))
+
+		res, _ := json.Marshal(req)
+		fmt.Println(string(res))
+
+		c.Status(200).Write(res)
+		return nil
+	})
+
+	app.Listen(":5000")
 }
